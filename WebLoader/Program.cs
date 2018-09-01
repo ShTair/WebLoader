c76c1cab6b10ca456883371e22b90d8766d2fc1e
ShTair <git@vsh.jp>
2018-09-02 02:33:28 +0900

    ファイルのロードを実装

--- WebLoader/Program.cs
+++ WebLoader/Program.cs
@@ -1,6 +1,8 @@
 ﻿using FluentFTP;
 using Newtonsoft.Json;
+using System;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebLoader
@@ -17,9 +19,61 @@ namespace WebLoader
             var param = await LoadParamAsync(paramFile);
 
             var client = new FtpClient(param.Host, param.UserName, param.Password);
+            client.DownloadDataType = FtpDataType.Binary;
+            client.Encoding = Encoding.Default;
             await client.ConnectAsync();
 
-            var items = await client.GetListingAsync("/", FtpListOption.AllFiles);
+            Directory.CreateDirectory(param.VaultPath);
+            try
+            {
+                await LoadDirectoriesAsync(client, param.BasePath, param.VaultPath);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+            }
+        }
+
+        private static async Task LoadDirectoriesAsync(FtpClient client, string path, string dstPath)
+        {
+            var items = await client.GetListingAsync(path, FtpListOption.AllFiles);
+            foreach (var item in items)
+            {
+                switch (item.Type)
+                {
+                    case FtpFileSystemObjectType.File:
+                        {
+                            Console.Write($"F: {item.FullName} ");
+                            var filePath = Path.Combine(dstPath, item.Name);
+                            if (!File.Exists(filePath) | (File.GetLastWriteTime(filePath) != item.Modified))
+                            {
+                                Console.WriteLine("DL");
+                                await client.DownloadFileAsync(filePath, item.FullName);
+                                File.SetLastWriteTime(filePath, item.Modified);
+                            }
+                            else
+                            {
+                                Console.WriteLine("NMOD");
+                            }
+                        }
+                        break;
+                    case FtpFileSystemObjectType.Directory:
+                        {
+                            Console.WriteLine($"D: {item.FullName}");
+                            var ndpath = Path.Combine(dstPath, item.Name);
+                            Directory.CreateDirectory(ndpath);
+
+                            await LoadDirectoriesAsync(client, item.FullName, ndpath);
+
+                            Directory.SetLastWriteTime(ndpath, item.Modified);
+                        }
+                        break;
+                    case FtpFileSystemObjectType.Link:
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
 
         private static async Task<TargetParam> LoadParamAsync(string paramFile)
--- WebLoader/TargetParam.cs
+++ WebLoader/TargetParam.cs
@@ -7,5 +7,9 @@
         public string UserName { get; set; }
 
         public string Password { get; set; }
+
+        public string BasePath { get; set; }
+
+        public string VaultPath { get; set; }
     }
 }
