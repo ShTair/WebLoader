924e3fed273d9518fdbc601b4c17654cafbb72af
ShTair <git@vsh.jp>
2018-09-02 01:45:17 +0900

    ファイルのリスト

--- WebLoader/Program.cs
+++ WebLoader/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FluentFTP;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace WebLoader
 {
@@ -6,7 +9,23 @@ namespace WebLoader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Run(args[0]).Wait();
+        }
+
+        private static async Task Run(string paramFile)
+        {
+            var param = await LoadParamAsync(paramFile);
+
+            var client = new FtpClient(param.Host, param.UserName, param.Password);
+            await client.ConnectAsync();
+
+            var items = await client.GetListingAsync("/", FtpListOption.AllFiles);
+        }
+
+        private static async Task<TargetParam> LoadParamAsync(string paramFile)
+        {
+            var json = await File.ReadAllTextAsync(paramFile);
+            return JsonConvert.DeserializeObject<TargetParam>(json);
         }
     }
 }
